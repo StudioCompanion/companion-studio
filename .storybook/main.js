18e924114647953ce8f0726fddb4e8c10b972c15
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-11-01 20:59:16 +0000

    chore(fix): storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['packages/web/src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
 }
