f7126e9e984fddf2cf877a3d36d2b7b67270e77a
dmaslan <danielmaslan@gmail.com>
2021-11-07 12:55:06 +0100

    feat: initial text section  setup

---
+++ packages/web/src/client/components/TextSection/TextSection.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { PADDING } from '../../styles/constants'
+
+const TextSection = ({ heading, children }) => {
+  return (
+    <Container>
+      <Heading>{heading}</Heading>
+      {children}
+    </Container>
+  )
+}
+
+TextSection.propTypes = {
+  children: PropTypes.node.isRequired,
+  heading: PropTypes.string,
+}
+
+export default TextSection
+
+const Container = styled.div`
+  width: 50%;
+`
+const Heading = styled.h6`
+  font-size: 1.2rem;
+  margin-bottom: ${PADDING[0]}px;
+`
---
+++ packages/web/src/client/styles/constants.js
@@ -0,0 +1,19 @@
+export const ASPECT_RATIOS = {
+  full: '56.25%',
+  full_mobile: '69%',
+  half: '114%',
+  half_mobile: '103%',
+  two_thirds: '85%',
+  two_thirds_mobile: '69%',
+}
+
+export const RADII = {
+  wrapper: 8,
+  wrapper_mobile: 4,
+  video_desktop: 4,
+  video_mobile: 2,
+}
+
+export const PADDING = [12]
+
+export const LAYOUTS = ['full', 'half', '2/3']
