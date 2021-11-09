02b1c98bb024fc52f6f38531b379a4d324c94fd5
dmaslan <55900387+dmaslan@users.noreply.github.com>
2021-11-09 19:25:36 +0100

    feat: Project info card (#33)
    * added global styles to storybook
    
    * feat: product info card
    
    * fix: changed padding back to pixels
    
    * fix: max-width
    
    * fix: added fonts, fixed styles
    
    * refactor: added blue text to global styles
    
    * fix: moved public folder
    
    * refactor: changed to dt tags, color constant to string

--- packages/web/src/client/styles/constants.js
+++ packages/web/src/client/styles/constants.js
@@ -1,3 +1,14 @@
+
+export const RADII = {
+  wrapper: 8,
+  wrapper_mobile: 4,
+  video_desktop: 4,
+  video_mobile: 2,
+}
+
+export const PADDING = [12, 16, 24]
+
 export const COLORS = {
-  darkblue: '#080B37',
+  lightgrey: '#F6F6F8',
+    darkblue: '#080B37',
 }
