ce3e34e1c07ad74c87b804525ed9ddc391febc39
dmaslan <danielmaslan@gmail.com>
2021-11-10 16:42:09 +0100

    chore: set up story

---
+++ packages/web/src/client/components/Nav/Nav.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Nav = () => {
+  return <div></div>
+}
+
+export default Nav
---
+++ packages/web/src/client/components/Nav/Nav.stories.js
@@ -0,0 +1,12 @@
+import Nav from './Nav'
+
+const Template = (args) => <Nav {...args} />
+
+export default {
+  title: 'Components/Nav',
+  component: Nav,
+}
+
+export const Primary = Template.bind({})
+
+// Primary.args =
