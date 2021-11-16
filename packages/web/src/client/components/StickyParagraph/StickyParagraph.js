fabc49f4c6d072ad37ccda242d306d22fbfc35c7
dmaslan <danielmaslan@gmail.com>
2021-11-16 12:36:01 +0100

    feat: styling

--- packages/web/src/client/components/StickyParagraph/StickyParagraph.js
+++ packages/web/src/client/components/StickyParagraph/StickyParagraph.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import Image from 'next/image'
 
-import { COLORS, RADII } from 'styles/constants'
+import { COLORS, RADII, PADDING } from 'styles/constants'
 import { MEDIA_QUERIES } from 'styles/mediaQueries'
 import { getFontStyles } from 'styles/getFontStyles'
 import {
@@ -9,39 +10,47 @@ import {
   FONT_STYLE_RECKLESS_12_400,
 } from 'styles/fonts'
 
-const StickyParagraph = ({}) => {
-  return <Container></Container>
+const StickyParagraph = ({ text, image }) => {
+  return (
+    <StickyParagraphContainer>
+      <StickyContainer>
+        <StickyText>{text}</StickyText>
+      </StickyContainer>
+      <ImageContainer>
+        <Image src={image.src} width={image.width} height={image.height} />
+      </ImageContainer>
+    </StickyParagraphContainer>
+  )
 }
 
-StickyParagraph.propTypes = {}
+StickyParagraph.propTypes = {
+  text: PropTypes.string,
+  image: PropTypes.object,
+}
 
 export default StickyParagraph
 
-const Container = styled.div``
-
-const paragraphs = [
-  {
-    text: 'We aim to enhance the lives of everyone around us by designing tools, services and products that inspire, entertain and create a better future for planet and people.',
-    image: '/images/graphics/approach/approach_create_change.png',
-  },
-  {
-    text: 'Whether you have the seed of an idea or you have been there and done it before, we work with you to create long-term value and fulfil your creative & business potential.',
-    image: '/images/graphics/approach/approach_extension_or_outside.png',
-  },
-  {
-    text: 'We can work as an extension of you, or take care of it all for you. We prefer long-term relationships, so we tend to speak our mind openly as after all, your interests are our interests.',
-    image: '/images/graphics/approach/approach_flexible_and_strong.png',
-  },
-  {
-    text: 'Every digital project is different and needs a flexible approach, so we focus on finding the most appropriate solutions for you, rather than trying to have you jump through hoops.',
-    image: '/images/graphics/approach/approach_dive_deep.png',
-  },
-  {
-    text: 'Having fun and playing is the best way to create. That’s why we don’t do free pitches, overwork our team or embrace the burnout culture of the creative industry. It’s not fun.',
-    image: '/images/graphics/approach/approach_trampoline.png',
-  },
-  {
-    text: 'Instead we partner with people that share our values: openness, curiosity, togetherness and a desire to do better for the people & world around us.',
-    image: '/images/graphics/approach/approach_pull_together.png',
-  },
-]
+const StickyParagraphContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 653px;
+  height: 770px;
+`
+const StickyContainer = styled.div`
+  flex-grow: 1;
+  position: relative;
+`
+const StickyText = styled.p`
+  text-align: center;
+  padding-top: ${PADDING.xl}px;
+  position: sticky;
+  top: 0;
+`
+const ImageContainer = styled.div`
+  padding-top: ${PADDING.xl}px;
+  min-height: 400px;
+  display: flex;
+  align-items: center;
+`
