166f6f449b8a5dcac598909e82f7b7348cff00e6
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-01-10 15:16:31 +0000

    feat: animate the people

--- packages/web/src/client/components/Avatars/Avatars.js
+++ packages/web/src/client/components/Avatars/Avatars.js
@@ -1,105 +1,15 @@
-import PropTypes from 'prop-types'
+import { useMemo, useRef } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
+import PropTypes from 'prop-types'
+import { useSprings, animated } from '@react-spring/web'
 
-import { PADDING, COLORS } from 'styles/constants'
+import { COLORS } from 'styles/constants'
 import { MEDIA_QUERIES } from 'styles/mediaQueries'
 import { getFontStyles } from 'styles/getFontStyles'
-import {
-  FONT_STYLE_APFEL_12_400,
-  FONT_STYLE_RECKLESS_12_400,
-  FONT_STYLE_RECKLESS_17_400,
-} from 'styles/fonts'
+import { FONT_STYLE_APFEL_12_400 } from 'styles/fonts'
 
 import FadeUp from 'components/Transitions/FadeUp'
-
-const GridItem = ({ image, name, role }) => {
-  return (
-    <FadeUp>
-      <GridItemContainer>
-        <GridImageWrapper>
-          <Image src={image} placeholder="blur" alt={name} />
-        </GridImageWrapper>
-        <TeamMemberName>{name}</TeamMemberName>
-        <TeamMemberRole>{role}</TeamMemberRole>
-      </GridItemContainer>
-    </FadeUp>
-  )
-}
-GridItem.propTypes = {
-  image: PropTypes.string,
-  name: PropTypes.string,
-  role: PropTypes.string,
-}
-
-const TeamGrid = ({}) => {
-  return (
-    <GridWrapper>
-      {team.map(({ image, name, role }, index) => (
-        <GridItem key={index} image={image} name={name} role={role} />
-      ))}
-    </GridWrapper>
-  )
-}
-
-TeamGrid.propTypes = {}
-
-export default TeamGrid
-
-const GridWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-start;
-  margin: 0px 0px 16px 0px;
-
-  ${MEDIA_QUERIES.tabletUp} {
-    width: 40%;
-    margin: 32px 0px 0px 0px;
-    justify-content: flex-end;
-  }
-`
-
-const GridItemContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin: 0px 0px 0px 4px;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:hover > h2 {
-    display: block;
-    width: auto;
-    height: 100%;
-  }
-`
-
-const GridImageWrapper = styled.div`
-  border-radius: 100%;
-  padding: 4px;
-  max-width: 40px;
-  background: ${COLORS.lightgrey_2};
-
-  & img {
-    top: 3px !important;
-    transition: 0.4s ease-out;
-  }
-`
-
-const TeamMemberName = styled.h2`
-  ${getFontStyles(FONT_STYLE_APFEL_12_400)}
-  width: auto;
-  display: none;
-  width: auto;
-  margin: 0px 8px 0px 8px;
-`
-
-const TeamMemberRole = styled.h3`
-  ${getFontStyles(FONT_STYLE_APFEL_12_400)}
-  display: none;
-`
 
 import myles from '../../../../public/images/team/myles.png'
 import elena from '../../../../public/images/team/elena.png'
@@ -108,7 +18,7 @@ import axelle from '../../../../public/images/team/axelle.png'
 import josh from '../../../../public/images/team/josh.png'
 import willem from '../../../../public/images/team/willem.png'
 
-const team = [
+const TEAM = [
   {
     name: 'Myles Palmer',
     role: 'Founder & Creative Director',
@@ -134,4 +44,121 @@ const team = [
     role: 'Digital Designer',
     image: willem,
   },
+  {
+    name: 'Elena Marinaki',
+    role: 'Developer',
+    image: elena,
+  },
 ]
+
+const Avatars = ({ names = [] }) => {
+  const textRefs = useRef([])
+
+  const members = useMemo(
+    () =>
+      names.length > 0
+        ? TEAM.filter((member) => names.includes(member.name))
+        : TEAM,
+    [names]
+  )
+
+  const [springs, api] = useSprings(
+    members.length,
+    () => ({
+      width: 0,
+    }),
+    [members]
+  )
+
+  const handleMouseEnter = (i) => () => {
+    api.start((j) => ({
+      width: j === i ? textRefs.current[i].scrollWidth + 8 : 0,
+    }))
+  }
+
+  const handleMouseLeave = (i) => () => {
+    api.start((j) => ({
+      width: 0,
+      delay: j === i ? 400 : 0,
+    }))
+  }
+
+  return (
+    <GridWrapper>
+      {members.map(({ image, name, role }, i) => (
+        <FadeUp key={name}>
+          <GridItemContainer
+            onMouseEnter={handleMouseEnter(i)}
+            onMouseLeave={handleMouseLeave(i)}
+          >
+            <GridImageWrapper>
+              <Image src={image} placeholder="blur" alt={name} />
+            </GridImageWrapper>
+            <TeamMemberDetails
+              ref={(ref) => (textRefs.current[i] = ref)}
+              style={{ width: springs[i].width }}
+            >
+              <TeamMemberName>{name}</TeamMemberName>
+              <TeamMemberRole>{role}</TeamMemberRole>
+            </TeamMemberDetails>
+          </GridItemContainer>
+        </FadeUp>
+      ))}
+    </GridWrapper>
+  )
+}
+
+Avatars.propTypes = {
+  names: PropTypes.arrayOf(PropTypes.string),
+}
+
+export default Avatars
+
+const GridWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  margin: 0px 0px 16px 0px;
+
+  ${MEDIA_QUERIES.tabletUp} {
+    width: 40%;
+    margin: 32px 0px 0px 0px;
+    justify-content: flex-end;
+  }
+`
+
+const GridItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+`
+
+const GridImageWrapper = styled.div`
+  border-radius: 100%;
+  padding: 4px;
+  width: 40px;
+  background: ${COLORS.lightgrey_2};
+
+  & img {
+    top: 3px !important;
+    transition: 0.4s ease-out;
+  }
+`
+
+const TeamMemberDetails = styled(animated.div)`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  margin-left: 8px;
+`
+
+const TeamMemberName = styled.h2`
+  ${getFontStyles(FONT_STYLE_APFEL_12_400)}
+  white-space: nowrap;
+`
+
+const TeamMemberRole = styled.h3`
+  ${getFontStyles(FONT_STYLE_APFEL_12_400)}
+  white-space: nowrap;
+`
