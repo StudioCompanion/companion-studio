35e4e58b4db5acee41a0177f9fd8c79f8fbd6a7e
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-03-22 16:21:34 +0000

    chore: removed package-lock and reinstalled

---
+++ packages/web/src/client/components/ImageStrip/ImageStrip.stories.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import { ImageStrip } from './ImageStrip'
+import { Story, Meta } from '@storybook/react'
+
+const Template: Story<JSX.IntrinsicAttributes> = (args) => (
+  <ImageStrip {...args} />
+)
+
+export const Primary = Template.bind({})
+
+Primary.args = {}
+
+export default {
+  title: 'Components/Image Strip',
+  component: ImageStrip,
+} as Meta
