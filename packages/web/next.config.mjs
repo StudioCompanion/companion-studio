b9205acf1557ff4d902fd111c714f17c7cef8523
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-04-27 16:27:08 +0100

    feat: add handling of redirects
    resolves #135

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,4 +1,10 @@
+import BundleAnalyzer from '@next/bundle-analyzer'
 import MDX from '@next/mdx'
+import groq from 'groq'
+
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const withMDX = MDX({
   options: {
@@ -13,6 +19,30 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ['cdn.sanity.io'],
+    minimumCacheTTL: 31536000,
+  },
+  redirects: async () => {
+    const REDIRECTS_QUERY = groq`
+        *[_type == 'settings'] | order(_updatedAt desc)[0].redirects[]{
+          destination,
+          permanent,
+          source,
+        }
+    `
+
+    try {
+      const { result } = await fetch(
+        `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/${process.env.NEXT_PUBLIC_SANITY_DATASET}?query=${REDIRECTS_QUERY}`
+      ).then((res) => res.json())
+
+      return result ?? []
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  },
 }
 
-export default withMDX(nextConfig)
+export default withBundleAnalyzer(withMDX(nextConfig))
