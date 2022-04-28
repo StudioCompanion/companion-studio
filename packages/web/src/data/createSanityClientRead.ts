18bc87f56ff86208a4cb448f0405ad629711124b
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-04-28 11:18:42 +0100

    chore: bring back token settings

--- packages/web/src/data/createSanityClientRead.ts
+++ packages/web/src/data/createSanityClientRead.ts
@@ -6,6 +6,5 @@ export const createSanityClientRead = (preview?: boolean) =>
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: '2022-02-18',
     useCdn: true,
-    // token: preview ? process.env.SANITY_CMS_TOKEN : undefined,
-    token: undefined,
+    token: preview ? process.env.SANITY_CMS_TOKEN : undefined,
   })
