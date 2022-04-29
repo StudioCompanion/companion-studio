4f8cd5c0ec4ad5ece6fc4f4baa39d3c2fa1e0b01
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-04-29 16:16:30 +0100

    feat: privacy schema added to cms

---
+++ packages/cms/schemas/singletons/privacy.js
@@ -0,0 +1,34 @@
+import { Lock } from 'phosphor-react'
+
+export default {
+  name: 'privacy',
+  title: 'Privacy',
+  type: 'document',
+  icon: Lock,
+  fields: [
+    {
+      name: 'faq',
+      title: 'FAQ',
+      type: 'array',
+      of: [
+        {
+          name: 'q_a',
+          title: 'Q & A',
+          type: 'object',
+          fields: [
+            {
+              name: 'question',
+              title: 'Question',
+              type: 'string',
+            },
+            {
+              name: 'answer',
+              title: 'Answer',
+              type: 'richText',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
