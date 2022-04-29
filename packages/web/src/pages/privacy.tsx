a4dfaec7bc42178ad1e70c7810b6519b324ecb56
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-04-29 18:27:29 +0100

    feat: privacy faq rendered on page

--- packages/web/src/pages/privacy.tsx
+++ packages/web/src/pages/privacy.tsx
@@ -11,6 +11,7 @@ import { REVALIDATE_TIME } from 'references/constants'
 import { PRIVACY_PAGE } from 'src/data/queries/singletons/privacy'
 
 import { Sanity } from 'src/types'
+import { RendererRichText } from 'components/Renderer/RendererRichText'
 
 interface PrivacyProps extends Sanity.DefaultLayoutProps {
   document: Sanity.PrivacyPage
@@ -24,13 +25,28 @@ const Privacy = ({ document, ...siteProps }: PrivacyProps) => {
   return (
     <Layout documentMeta={meta} {...siteProps}>
       <PrivacyWrapper>
-        <PrivacyContainer>Hello</PrivacyContainer>
+        <PrivacyContainer>
+          {faq?.map((item, idx) => (
+            <>
+              <Question key={idx}>{item.question}</Question>
+              <Answer blocks={item.answer}></Answer>
+            </>
+          ))}
+        </PrivacyContainer>
       </PrivacyWrapper>
     </Layout>
   )
 }
 
 export default Privacy
+
+const Question = styled.h2`
+  color: olivedrab;
+`
+
+const Answer = styled(RendererRichText)`
+  color: burlywood;
+`
 
 export const getStaticProps: GetStaticProps<PrivacyProps> = async ({
   preview,
