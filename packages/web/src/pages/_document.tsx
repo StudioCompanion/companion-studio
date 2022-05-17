6a33e3e786d905ccd61e550f9c59f72c1a60e6b7
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-05-17 17:26:47 +0100

    fix: link href and rel

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -52,11 +52,11 @@ export default class Doc extends Document {
           <link rel="shortcut icon" href={'favicon/favicon.ico'} />
           <link rel="apple-touch-icon" href={'favicon/apple-touch-icon.png'} />
           <link
-            rel="favicon/android-chrome-192x192"
+            rel="android-chrome-192x192"
             href={'favicon/android-chrome-192x192.png'}
           />
           <link
-            rel="favicon/android-chrome-256x256"
+            rel="android-chrome-256x256"
             href={'favicon/android-chrome-256x256.png'}
           />
           <link rel="mask-icon" href={'favicon/safari-pinned-tab.svg'} />
