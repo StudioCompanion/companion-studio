149dc88734c78e66b37e183c91f39731abda6591
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-07-07 15:57:11 +0100

    chore: variations to buttons

--- packages/web/src/client/components/Button/Button.stories.tsx
+++ packages/web/src/client/components/Button/Button.stories.tsx
@@ -6,17 +6,34 @@ import { StoryFixtures } from '@types'
 import { ThemeTypes } from 'styles/constants'
 
 export default {
-  title: 'Buttons/Button',
+  title: 'Button',
   component: Button,
 } as ComponentMeta<typeof Button>
 
+export const Dark: ComponentStory<typeof Button> = () => (
+  <Button {...fixtures.dark} />
+)
+
+export const Light: ComponentStory<typeof Button> = () => (
+  <Button {...fixtures.light} />
+)
+
 export const Default: ComponentStory<typeof Button> = () => (
-  <Button {...fixtures.default} />
+  <Button {...fixtures.span} />
 )
 
 const fixtures: StoryFixtures<typeof Button> = {
-  default: {
+  dark: {
     text: `Click Me!`,
     theme: ThemeTypes.DARK,
+  },
+  light: {
+    text: `Click Me!`,
+    theme: ThemeTypes.LIGHT,
+  },
+  span: {
+    text: `Click Me!`,
+    theme: ThemeTypes.DARK,
+    tag: `span`,
   },
 }
