8f4813cd1f14a64905febb3ff7913d2db9eb2136
Myles Palmer <myles@companion.studio>
2025-07-16 22:32:34 +0100

    Link fix

--- packages/web/src/client/components/Site/HiatusBanner.tsx
+++ packages/web/src/client/components/Site/HiatusBanner.tsx
@@ -7,7 +7,7 @@ export const HiatusBanner = () => {
       <BannerContent>
         <BannerText tag="p" fontStyle="XS">
           We're currently on hiatus and not taking new projects, but you can still{' '}
-          <BannerLink href="mailto:hello@companion.studio">
+          <BannerLink href="https://www.mylespalmer.co.uk">
             reach out our founder
           </BannerLink>
           .
